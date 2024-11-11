da9d5ca2798961794e3446a5235cae6018ecfd0a
Jai Bagaria <deviljai1999@gmail.com>
2024-11-11 16:12:30 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express = require('express')
 const app = require('./src/app');
 const mongoose = require('mongoose')
 const cors = require('cors');
-app.use(cors());
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({
     origin: 'https://youtube-subscriber-kappa.vercel.app/',
     methods: ['GET'],
     credentials: true,
   }));
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+
 
 
 const DATABASE_URL = "mongodb+srv://deviljai1999:RiMoO4FKfdrqw1La@cluster0.fijzqch.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
