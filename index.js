4f13fb6515ef2c309b2a1497aff3d0565dd2e944
Devil-jai <140802260+Devil-jai@users.noreply.github.com>
2024-11-11 12:44:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({
-    origin: 'http://localhost:2001/'
+    origin: 'https://youtube-subscriber-kappa.vercel.app/'
   }));
 
 
