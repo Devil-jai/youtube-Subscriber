8e3ac1fe97d1cd404b236ab0e850f770c48b8874
Jai Bagaria <deviljai1999@gmail.com>
2024-11-14 13:51:40 +0530

    update database

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 
 
 const DATABASE_URL = "mongodb+srv://deviljai1999:OGLMkuGA31Pp1FSM@cluster0.inasx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(DATABASE_URL);
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
 db.once('open', () => console.log('connected to database'))
--- src/createDatabase.js
+++ src/createDatabase.js
@@ -4,7 +4,7 @@ const data = require('./data')
 
 
 const DATABASE_URL = "mongodb+srv://deviljai1999:OGLMkuGA31Pp1FSM@cluster0.inasx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(DATABASE_URL);
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
 db.once('open', () => console.log('Database created...'))
