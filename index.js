cb85b7bf68269fcdf71e1a683481c406babef4f9
Jai Bagaria <deviljai1999@gmail.com>
2024-11-14 20:17:50 +0530

    add

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ app.use(express.urlencoded({ extended: false }));
 
 
 const DATABASE_URL = "mongodb+srv://deviljai1999:OGLMkuGA31Pp1FSM@cluster0.inasx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(DATABASE_URL);
+mongoose.connect(DATABASE_URL, {
+  tls: true,
+  tlsAllowInvalidCertificates: true, // Allows invalid certificates, bypassing SSL validation
+});
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
 db.once('open', () => console.log('connected to database'))
