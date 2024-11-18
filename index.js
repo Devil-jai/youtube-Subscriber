75a66eb60dee34ea4255993ecab8e8a00d5fc9c8
Jai Bagaria <deviljai1999@gmail.com>
2024-11-18 16:11:40 +0530

    dd

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = require('./src/app');
 const mongoose = require('mongoose')
-const cors = require('cors');
+// const cors = require('cors');
+const port = process.env.port || 4001;
 
-
-app.use(cors({
-    origin: 'https://youtube-subscriber-kappa.vercel.app/',
-    methods: ['GET'],
-    credentials: true,
-  }));
+// app.use(cors({
+//     origin: 'https://youtube-subscriber-kappa.vercel.app/',
+//     methods: ['GET'],
+//     credentials: true,
+//   }));
 
 
 app.use(express.json())
@@ -17,17 +17,13 @@ app.use(express.urlencoded({ extended: false }));
 
 
 const DATABASE_URL = "mongodb+srv://deviljai1999:OGLMkuGA31Pp1FSM@cluster0.inasx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(DATABASE_URL, {
-  tls: true,
-  tlsAllowInvalidCertificates: true, 
-  tlsInsecure: true, // Allows invalid certificates, bypassing SSL validation
-});
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
 db.once('open', () => console.log('connected to database'))
 
 
-app.listen(2001, () => console.log(`App listening on port 2001`))
+app.listen(port, () => console.log(`App listening on port ${port}`))
 
 
 
