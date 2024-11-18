acd8fbf6b970475502bd5b235003267d5ebda43f
Jai Bagaria <deviljai1999@gmail.com>
2024-11-18 16:19:37 +0530

    ii

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./index.html'))
+    res.sendFile(path.join(__dirname,'/index.html'))
 })
 
 app.get("/subscribers", async (req, res, next) => {
