2e5cf985bd8de785f2ae1b9b59bc5e4a61aab2a0
Jai Bagaria <deviljai1999@gmail.com>
2024-11-18 17:59:03 +0530

    10s

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ app.use(express.urlencoded({ extended: false }));
 
 
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true,
+   useUnifiedTopology: true ,
+   serverSelectionTimeoutMS: 10000, });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
 db.once('open', () => console.log('connected to database'))
